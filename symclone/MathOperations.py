a9ccd96088b0a384ba7a58410927041a53ef7bb6
CH6832 <147909141+CH6832@users.noreply.github.com>
2024-05-27 11:34:52 +0200

    Update MathOperations.py

--- symclone/MathOperations.py
+++ symclone/MathOperations.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-"""FuzzyBoolean.py
+"""MathOperations.py
 
-Example usage of the SymClone library.
+Mathematical operations for the SymClone library.
 """
 
 # https://docs.pylint.org/
